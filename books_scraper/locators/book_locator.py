0552aeb248be2d5240d359a010e57fd0a94fcc07
jorge <jorgezafra94@hotmail.com>
2024-02-18 19:35:39 -0600

    add book locators

---
+++ books_scraper/locators/book_locator.py
@@ -0,0 +1,6 @@
+class BookLocator:
+    BOOK_TITLE = 'article.product_pod h3 a'
+    BOOK_PRICE = 'article.product_pod div.product_price p.price_color'
+    BOOK_AVAILABILITY = 'article.product_pod div.product_price p.instock'
+    BOOK_STARTS = 'article.product_pod p.star-rating'
+
---
+++ books_scraper/locators/book_page_locator.py
@@ -0,0 +1,4 @@
+class BookPageLocator:
+    BOOK = 'div.page_inner section li.col-xs-6'
+    NEXT_PAGE = 'div.page_inner section ul.pager li.next a'
+
