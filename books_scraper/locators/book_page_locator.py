0bfec7324832cb145fa4abe48dfbe0e13a649f84
jorge <jorgezafra94@hotmail.com>
2024-02-20 12:44:20 -0600

    improving Pager locator

--- books_scraper/locators/book_page_locator.py
+++ books_scraper/locators/book_page_locator.py
@@ -1,4 +1,4 @@
 class BookPageLocator:
     BOOK = 'div.page_inner section li.col-xs-6'
-    NEXT_PAGE = 'div.page_inner section ul.pager li.next a'
+    PAGER = 'div.page_inner section ul.pager li.current'
 
