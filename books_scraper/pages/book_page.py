af2d9301d34a57ebd655f63821e5a913fb26a0da
jorge <jorgezafra94@hotmail.com>
2024-02-20 14:30:06 -0600

    improving book page using BookRegex to get info

--- books_scraper/pages/book_page.py
+++ books_scraper/pages/book_page.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from locators.book_page_locator import BookPageLocator
 from parsers.book_parser import BookParser
+from regex.book_regex import BookRegex
 
 
 class BookPage:
@@ -8,15 +9,17 @@ class BookPage:
     def __init__(self, raw_page):
         self.soup_page = BeautifulSoup(raw_page, 'html.parser')
 
-    def get_books(self):
+    @property
+    def books(self):
         locator = BookPageLocator.BOOK
         list_li_tags = self.soup_page.select(locator)
         list_books = [BookParser(elem) for elem in list_li_tags]
         return list_books
 
-    def get_next_page(self):
-        locator = BookPageLocator.NEXT_PAGE
-        a_tag = self.soup_page.select_one(locator)
-        if a_tag:
-            return a_tag.attrs.get('href')
-        return None
+    @property
+    def pages(self):
+        locator = BookPageLocator.PAGER
+        li_tag = self.soup_page.select_one(locator)
+        raw_text = li_tag.string.strip()
+        return int(BookRegex.get_pages(raw_text))
+
