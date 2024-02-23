33bc0023a93b19e5c72834866f44dba33ae037ea
jorge <jorgezafra94@hotmail.com>
2024-02-22 19:20:51 -0600

    adding locators

---
+++ selenium_quotes_scraper/locators/quote_locator.py
@@ -0,0 +1,6 @@
+class QuoteLocator:
+    AUTHOR = 'small.author'
+    CONTENT = 'span.text'
+    TAGS = 'div.tags a.tag'
+
+
---
+++ selenium_quotes_scraper/locators/quote_page_locator.py
@@ -0,0 +1,5 @@
+class QuotePageLocator:
+    QUOTE = 'div.quote'
+    NEXT_PAGE = 'nav ul.pager li.next a'
+
+
